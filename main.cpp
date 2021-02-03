3796b37097b1cb44c9246b21a1303cd5aa4f9f2a
drams567 <44892153+drams567@users.noreply.github.com>
2021-02-02 23:09:36 -0500

    Space class added, board initialization added, print board function added. Empty space value now a period.

--- main.cpp
+++ main.cpp
@@ -6,8 +6,64 @@ using namespace std;
 
 const int N = 9;
 const int NUM_SPACES = N*N;
+const int X = 0;
+const int Y = 1;
 
-int main(int argc, char* argv[])
+class Space {
+private:
+	int symbol;
+	bool fixed;
+	int index[2];
+
+public:
+	Space(int x, int y)
+	{
+		symbol = -1;
+		fixed = false;
+		index[X] = x;
+		index[Y] = y;
+	}
+	
+	Space(int x, int y, int newSymbol)
+	{
+		symbol = newSymbol;
+		fixed = true;
+		index[X] = x;
+		index[Y] = y;
+	}
+	
+	~Space(){}
+};
+
+void printBoard(string boardString)
+{
+	cout << endl;
+	cout << string(N*3 + 5, '-') << endl;
+	for(int i = 0; i < N; i++)
+	{
+		for(int k = 0; k < N; k++)
+		{
+			if(k % 3 == 0)
+			{
+				cout << "|";
+			}
+			cout << " " << boardString[(i*N + k)] << " ";
+		}
+		cout << " |" << endl;
+		if(i % 3 == 2)
+		{
+			cout << string(N*3 + 5, '-') << endl;
+		}
+		else
+		{
+			cout << endl;
+		}
+	}
+	
+	cout << endl;
+}
+
+int main(const int argc, const char* argv[])
 {	
 	// ARGS: run, board string
 	if(argc < 2)
@@ -28,13 +84,37 @@ int main(int argc, char* argv[])
 	boardStream >> boardString;
 	boardStream.close();
 
-	cout << boardString << endl;
 	if(boardString.length() != NUM_SPACES)
 	{
 		cout << endl << "main.cpp: Board string is invalid. Exiting..." << endl;
 		exit(0);
 	}
+
+	// print board
+	printBoard(boardString);
+
+	// Initialize board
+	Space* board[N][N];
+	for(int i = 0; i < N; i++)
+	{
+		for(int k = 0; k < N; k++)
+		{
+			board[i][k] = new Space(i, k);
+		}
+	}
 	
+	// Clean board
+	for(int i = 0; i < N; i++)
+	{
+		for(int k = 0; k < N; k++)
+		{
+			if(board[i][k] != NULL)
+			{
+				delete board[i][k];
+				board[i][k] = NULL;
+			}
+		}
+	}
 	
 	return 0;
 }
